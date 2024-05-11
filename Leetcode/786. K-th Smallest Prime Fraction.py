7bee1291a11042774528bdc74c69877481a918d3
Elutabe Efezino McCarthy <elutabeefezino@gmail.com>
2024-05-11 14:44:19 +0100

    made solution faster

--- Leetcode/786. K-th Smallest Prime Fraction.py
+++ Leetcode/786. K-th Smallest Prime Fraction.py
@@ -1,22 +1,33 @@
 class Solution:
     def kthSmallestPrimeFraction(self, arr: List[int], k: int) -> List[int]:
+        l = 0
+        r = 1
         n = len(arr)
-        q = []
 
-        # store the smallest possible fraction for each element as a numerator in a heap
-        for i in range(n-1):
-            heapq.heappush(q, (arr[i]/arr[-1], i, n-1))
-        
-        while k - 1:
-            # pop the smallest of the smallest set stored in the heap
-            _, i, j = heapq.heappop(q)
+        # perform a binary search over the range of 0 to 1
+        while l < r:
+            m = (l + r)/2 
+            # keep track of how many valid paris of fractions are below the current m
+            cnt = 0
+            j = 1
+            # keep track of the fraction pair closest to  the current m
+            maxF =  0
 
-            # push the nest smallest fraction with that element as the numerator
-            if i < j - 1:
-                heapq.heappush(q, (arr[i] / arr[j - 1], i, j - 1))
-            
-            # do this k-1 times
-            k -= 1
-        
-        # return the smallest fraction
-        return [arr[q[0][1]], arr[q[0][2]]]
+            for i in range(n - 1):
+                while j < n:
+                    if arr[i] / arr[j] > m:
+                        j += 1
+                    else:
+                        cnt += n - j
+                        if arr[i] / arr[j] > maxF:
+                            p = i
+                            q = j
+                            maxF = arr[i] / arr[j]
+                        break
+            # update the binary search based on count of elements less than m and return when equal to k
+            if cnt > k:
+                r = m
+            elif cnt < k:
+                l = m
+            else:
+                return arr[p], arr[q]
