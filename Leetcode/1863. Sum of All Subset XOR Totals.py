6247c9a806023e38851b47801ee8ed6ab125c89a
Elutabe Efezino McCarthy <elutabeefezino@gmail.com>
2024-05-21 00:23:04 +0100

    More efficient solution using combinatorics

--- Leetcode/1863. Sum of All Subset XOR Totals.py
+++ Leetcode/1863. Sum of All Subset XOR Totals.py
@@ -1,14 +1,21 @@
 class Solution(object):
     def subsetXORSum(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+    #     """
+    #     :type nums: List[int]
+    #     :rtype: int
+    #     """
 
-        def dfs(i, total):
-            if i == len(nums):
-                return total
+    #     def dfs(i, total):
+    #         if i == len(nums):
+    #             return total
             
-            return dfs(i + 1, total ^ nums[i]) + dfs(i + 1, total)
+    #         return dfs(i + 1, total ^ nums[i]) + dfs(i + 1, total)
         
-        return dfs(0,0)
+    #     return dfs(0,0)
+
+        res = 0
+
+        for num in nums:
+            res = res | num
+        
+        return res * 2 ** (len(nums) - 1)
